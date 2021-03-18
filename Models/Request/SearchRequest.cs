dcb9df557a73b18085a6b927e6952c12b82d3c92
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-03-18 14:45:56 +0000

    Finalised search query parameters, and search by age

--- Models/Request/SearchRequest.cs
+++ Models/Request/SearchRequest.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+// using ZooManagement.Models.Database;
+// using ZooManagement.Models.Request;
+// using ZooManagement.Models.Response;
+// using ZooManagement.Repositories;
+using System;
+// using System.Text;
+
 namespace ZooManagement.Models.Request
 {
     public class SearchRequest
@@ -9,16 +17,36 @@ namespace ZooManagement.Models.Request
 
     public class AnimalSearchRequest : SearchRequest
     {
-        private string _search;
-
-        public string Search
-        {
-            get => _search?.ToLower();
-            set => _search = value;
+        private string name;
+        [FromQuery(Name = "name")]
+        public string Name 
+        { 
+            get { return name == null ? null : name.ToLower(); }
+            set { name = value; }
         }
 
-        public override string Filters => Search == null ? "" : $"&search={Search}";
+        [FromQuery(Name = "age")]
+        public int? Age { get; set; }
 
-        
+        [FromQuery(Name = "acquired")]
+        public string DateAcquired { get; set; }
+
+        private string animalClass;
+        [FromQuery(Name = "class")]
+        public string Class 
+        {
+            get { return animalClass == null ? null : animalClass.ToLower();}
+            set { animalClass = value; }
+        }
+
+        private string alias;
+        [FromQuery(Name = "species")]
+        public string Alias 
+        { 
+            get { return alias == null ? null : alias.ToLower(); }
+            set { alias = value; }
+        }
+        public string Search => $"{Name}{Age}{DateAcquired}{Class}{Alias}";
+        public override string Filters => Search == null ? "" : $"{(Name == null ? "" : $"&name={Name}")}{(Age == null ? "" : $"&age={Age}")}{(DateAcquired == null ? "" : $"&acquired={DateAcquired}")}{(Class == null ? "" : $"&class={Class}")}{(Alias == null ? "" : $"&species={Alias}")}";  
     }
 }
--- Repositories/AnimalsRepo.cs
+++ Repositories/AnimalsRepo.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 using System.Linq;
 using ZooManagement.Models.Database;
 using ZooManagement.Models.Request;
@@ -44,19 +45,40 @@ namespace ZooManagement.Repositories
             _context.SaveChanges();
             return insertResponse.Entity;
         }
-
-         public IEnumerable<Animal> Search(AnimalSearchRequest search)
+        public IEnumerable<Animal> Search(AnimalSearchRequest search)
         {
+            // search.Name search.Age search.DateAcquired search.Class search.Alias
+            // return _context.Animals
+            //     .Include(a => a.AnimalType)
+            //     .Where(a => search.Search == null || 
+            //                 (
+            //                     a.Name.ToLower().Contains(search.Search) ||
+            //                     a.AnimalType.Alias.ToLower().Contains(search.Search) ||
+            //                     a.AnimalType.Class.ToLower().Contains(search.Search) ||
+            //                     a.AnimalType.Genus.ToLower().Contains(search.Search) ||
+            //                     a.AnimalType.Species.ToLower().Contains(search.Search) ||
+            //                     a.AnimalType.Family.ToLower().Contains(search.Search) 
+            //                 ))
+            //     .OrderBy(a => a.AnimalType.Class)
+            //     .Skip((search.Page - 1) * search.PageSize)
+            //     .Take(search.PageSize);
+
+            // int CalculateAge(DateTime birthday)
+            // {
+            //     // DateTime birthday = DateOfBirth;
+            //     int age = DateTime.Today.Year - birthday.Year;
+            //     if (birthday.Date > DateTime.Today.AddYears(-age)) { age--; }
+            //     return age;
+            // }  
+
             return _context.Animals
                 .Include(a => a.AnimalType)
                 .Where(a => search.Search == null || 
                             (
-                                a.Name.ToLower().Contains(search.Search) ||
-                                a.AnimalType.Alias.ToLower().Contains(search.Search) ||
-                                a.AnimalType.Class.ToLower().Contains(search.Search) ||
-                                a.AnimalType.Genus.ToLower().Contains(search.Search) ||
-                                a.AnimalType.Species.ToLower().Contains(search.Search) ||
-                                a.AnimalType.Family.ToLower().Contains(search.Search) 
+                                (search.Name == null || a.Name.ToLower().Contains(search.Name)) &&
+                                (search.Age == null || (a.DateOfBirth > DateTime.Today.AddYears(a.DateOfBirth.Year - DateTime.Today.Year) ? DateTime.Today.Year - a.DateOfBirth.Year - 1 : DateTime.Today.Year - a.DateOfBirth.Year) == search.Age) &&
+                                (search.Class == null || a.AnimalType.Class.ToLower().Contains(search.Class)) &&
+                                (search.Alias == null || a.AnimalType.Alias.ToLower().Contains(search.Alias)) 
                             ))
                 .OrderBy(a => a.AnimalType.Class)
                 .Skip((search.Page - 1) * search.PageSize)
@@ -69,13 +91,22 @@ namespace ZooManagement.Repositories
                 .Include(a => a.AnimalType)
                 .Count(a => search.Search == null || 
                             (
-                                a.Name.ToLower().Contains(search.Search) ||
-                                a.AnimalType.Class.ToLower().Contains(search.Search) ||
-                                a.AnimalType.Family.ToLower().Contains(search.Search) ||
-                                a.AnimalType.Genus.ToLower().Contains(search.Search) ||
-                                a.AnimalType.Species.ToLower().Contains(search.Search) ||
-                                a.AnimalType.Alias.ToLower().Contains(search.Search)
+                                (search.Name == null || a.Name.ToLower().Contains(search.Name)) &&
+                                (search.Class == null || a.AnimalType.Class.ToLower().Contains(search.Class)) &&
+                                (search.Alias == null || a.AnimalType.Alias.ToLower().Contains(search.Alias)) 
                             ));
+
+            // return _context.Animals
+            //     .Include(a => a.AnimalType)
+            //     .Count(a => search.Search == null || 
+            //                 (
+            //                     a.Name.ToLower().Contains(search.Search) ||
+            //                     a.AnimalType.Class.ToLower().Contains(search.Search) ||
+            //                     a.AnimalType.Family.ToLower().Contains(search.Search) ||
+            //                     a.AnimalType.Genus.ToLower().Contains(search.Search) ||
+            //                     a.AnimalType.Species.ToLower().Contains(search.Search) ||
+            //                     a.AnimalType.Alias.ToLower().Contains(search.Search)
+            //                 ));
         }
     }
 }
