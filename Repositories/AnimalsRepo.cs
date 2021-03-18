3adf7bdcfe7d708e077ac44260f54abb12494f13
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-03-18 10:58:46 +0000

    Found Nemo and Dory

--- Repositories/AnimalsRepo.cs
+++ Repositories/AnimalsRepo.cs
@@ -2,7 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using ZooManagement.Models.Database;
 using ZooManagement.Models.Request;
-using ZooManagement.Models.Response;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ZooManagement.Repositories
 {
@@ -26,6 +27,7 @@ namespace ZooManagement.Repositories
         public Animal GetById(int id)
         {
             return _context.Animals
+                .Include(animal => animal.AnimalType)
                 .Single(animal => animal.Id == id);
         }
 
@@ -37,7 +39,7 @@ namespace ZooManagement.Repositories
                 Sex = newAnimal.Sex,
                 DateOfBirth = newAnimal.DateOfBirth,   
                 AcquirementDate = newAnimal.AcquirementDate,
-                AnimalTypeId = newAnimal.AnimalTypeId         
+                AnimalTypeId = newAnimal.AnimalTypeId,         
             });
             _context.SaveChanges();
             return insertResponse.Entity;
@@ -46,14 +48,15 @@ namespace ZooManagement.Repositories
          public IEnumerable<Animal> Search(AnimalSearchRequest search)
         {
             return _context.Animals
-                .Where(p => search.Search == null || 
+                .Include(a => a.AnimalType)
+                .Where(a => search.Search == null || 
                             (
-                                p.Name.ToLower().Contains(search.Search) ||
-                                p.AnimalType.Alias.ToLower().Contains(search.Search) ||
-                                p.AnimalType.Class.ToLower().Contains(search.Search) ||
-                                p.AnimalType.Genus.ToLower().Contains(search.Search) ||
-                                p.AnimalType.Species.ToLower().Contains(search.Search) ||
-                                p.AnimalType.Family.ToLower().Contains(search.Search) 
+                                a.Name.ToLower().Contains(search.Search) ||
+                                a.AnimalType.Alias.ToLower().Contains(search.Search) ||
+                                a.AnimalType.Class.ToLower().Contains(search.Search) ||
+                                a.AnimalType.Genus.ToLower().Contains(search.Search) ||
+                                a.AnimalType.Species.ToLower().Contains(search.Search) ||
+                                a.AnimalType.Family.ToLower().Contains(search.Search) 
                             ))
                 .OrderBy(a => a.AnimalType.Class)
                 .Skip((search.Page - 1) * search.PageSize)
@@ -63,14 +66,15 @@ namespace ZooManagement.Repositories
         public int Count(AnimalSearchRequest search)
         {
             return _context.Animals
-                .Count(p => search.Search == null || 
+                .Include(a => a.AnimalType)
+                .Count(a => search.Search == null || 
                             (
-                                p.Name.ToLower().Contains(search.Search) ||
-                                p.AnimalType.Class.ToLower().Contains(search.Search) ||
-                                p.AnimalType.Family.ToLower().Contains(search.Search) ||
-                                p.AnimalType.Genus.ToLower().Contains(search.Search) ||
-                                p.AnimalType.Species.ToLower().Contains(search.Search) ||
-                                p.AnimalType.Alias.ToLower().Contains(search.Search)
+                                a.Name.ToLower().Contains(search.Search) ||
+                                a.AnimalType.Class.ToLower().Contains(search.Search) ||
+                                a.AnimalType.Family.ToLower().Contains(search.Search) ||
+                                a.AnimalType.Genus.ToLower().Contains(search.Search) ||
+                                a.AnimalType.Species.ToLower().Contains(search.Search) ||
+                                a.AnimalType.Alias.ToLower().Contains(search.Search)
                             ));
         }
     }
